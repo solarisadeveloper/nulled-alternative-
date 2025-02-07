601a4be5c87569618adb6ebc997a47d02d50e966
solarisadeveloper <damon.french8@gmail.com>
2025-02-08 09:22:15 +1100

    Update

---
+++ index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>My Next.js Website</title>
+      </Head>
+      <h1>Welcome to My Website</h1>
+      <p>This is a marketplace, forum, and downloads platform.</p>
+    </div>
+  );
+}
